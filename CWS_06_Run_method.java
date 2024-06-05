a6ec88f1521f1092e5421a423d5f78549dcd82fa
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-06-05 12:55:30 +0530

    run method of Threads

--- CWS_06_Run_method.java
+++ CWS_06_Run_method.java
@@ -1,4 +1,10 @@
 public class CWS_06_Run_method extends Thread{
+
+    /*
+        Thread If this thread was constructed using a separate Runnable run object, then that Runnable object's run method is called; otherwise, this method does nothing and returns.
+        Subclasses of Thread should override this method.
+     */
+
     @Override
     public void run(){
         System.out.println("Run method");
