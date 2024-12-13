3de007b00fcd2ec576c9b8da8f223f951f21cbc3
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-12-13 12:55:30 +0530

    MultiThreading and Main Thread in java

---
+++ Main_Thread.java
@@ -0,0 +1,11 @@
+public class Main_Thread {
+
+    /// so by default main thread will run when main method runs in our application
+    ///
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("current Thread Name: "+Thread.currentThread().getName());
+        System.out.println("current thread ID: "+Thread.currentThread().getId());
+    }
+}
